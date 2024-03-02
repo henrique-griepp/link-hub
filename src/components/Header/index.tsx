6880df488a22ea651daa6f221f380338037951f9
Henrique Griepp <henriquegriepp@DESKTOP-SU6BTHJ>
2024-03-02 02:17:57 -0300

    modified:   src/components/Header/index.tsx --Theme menu positioning fix 	modified:   src/pages/Home/index.tsx --Adding transform hover to social icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,9 @@
 import { ModeToggle } from "../mode-toggle.tsx" 
-// import "./style.css"
+
 
 export function Header () {
     return(
-        <nav className="p-2 container absolute flex justify-end ">
+        <nav className="p-2 container max-w-4xl absolute right-0 left-0 flex justify-end ">
             <ModeToggle />
         </nav> 
     )
