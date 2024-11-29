d25b2d3dbc85bcbaeea6914683a2bc93603c2dcd
Henrique Griepp Cerqueira <104803272+henrique-griepp@users.noreply.github.com>
2024-11-28 23:58:25 -0300

    Chore(cleanup): Removal of the email field.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,7 +13,7 @@ export function Home (){
         <img className="m-6 w-40 rounded-full shadow-lg transform transition-transform scale-100 hover:scale-110" src="https://avatars.githubusercontent.com/u/104803272?v=4" alt="photo perfil" />
         <p className="m-6 max-w-lg text-center">Oi, eu sou o Henrique! Desenvolvedor Web apaixonado por transformar ideias em soluções inovadoras.</p>
         <div className=" m-2 flex flex-auto justify-around h-14 w-80">
-           <a className="size-11 m-auto transform transition-transform scale-100 hover:scale-125" href="mailto:contato@grceh.com" target='_blank'><IconEmail /></a>
+           {/* <a className="size-11 m-auto transform transition-transform scale-100 hover:scale-125" href="mailto:contato@grceh.com" target='_blank'><IconEmail /></a> */}
            <a className="size-10 m-auto transform transition-transform scale-100 hover:scale-125" href='https://github.com/henrique-griepp' target='_blank'><IconGithub/></a>
            <a className="size-10 m-auto transform transition-transform scale-100 hover:scale-125" href='https://www.instagram.com/henriquegc.dev/' target='_blank'><IconInstagram/></a>
            <a className="size-10 m-auto transform transition-transform scale-100 hover:scale-125" href='https://www.linkedin.com/in/henriquegriepp/' target='_blank'><IconLinkedin/></a>
